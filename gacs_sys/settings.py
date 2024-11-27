37a15a3cf5dbc2922ddc61e848b7e6395459d4dc
sh1baruuu <devillarudolphangelo@gmail.com>
2024-11-28 03:11:58 +0800

    updated

--- gacs_sys/settings.py
+++ gacs_sys/settings.py
@@ -150,8 +150,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    BASE_DIR / "staticfiles",  # This duplicates STATIC_ROOT
+]
+
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
